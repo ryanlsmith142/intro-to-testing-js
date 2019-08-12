148cbcf3a7a92b7390e95b53379288340281218e
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-12 09:20:45 -0500

    add tests and ability to say "Hello" to any input."

--- code.js
+++ code.js
@@ -3,9 +3,10 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return "";
+function sayHello(input) {
+    return "Hello, " + input + "!";
 }
+
 function sum(a, b) {
     if(b === "sheep") {
         return false;
